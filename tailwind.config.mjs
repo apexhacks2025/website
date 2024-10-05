a7adb56cf6169db1e2734da57311f2aafcb566e9
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-10-04 20:55:37 -0400

    prettier

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,19 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
       colors: {
         apexblue: "#175CBC",
-        lessblue: "#2F62A8"
+        lessblue: "#2F62A8",
       },
       fontFamily: {
-        sans: ['Outfit', ...defaultTheme.fontFamily.sans],
-        display: ['Bruno Ace SC', ...defaultTheme.fontFamily.serif]
-      }
+        sans: ["Outfit", ...defaultTheme.fontFamily.sans],
+        display: ["Bruno Ace SC", ...defaultTheme.fontFamily.serif],
+      },
     },
-	},
-	plugins: [],
-}
+  },
+  plugins: [],
+};
