6873a537741c7993d7a67f8b51b85c06060f0245
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-10-04 21:32:42 -0400

    empty proposal resources page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 import defaultTheme from "tailwindcss/defaultTheme";
+import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -15,5 +16,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [typography],
 };
